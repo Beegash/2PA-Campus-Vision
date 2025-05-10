397204c30a08f883649dd5d3a2bb9ca4191a42c6
DaringUnicorn <156504496+daringStu@users.noreply.github.com>
2025-05-10 23:10:29 +0300

    occupancy urls.py change made

--- campusvision/occupancy/urls.py
+++ campusvision/occupancy/urls.py
@@ -3,4 +3,6 @@ from .views import RealTimeOccupancyAPIView
 
 urlpatterns = [
     path("api/real-time/", RealTimeOccupancyAPIView.as_view(), name="real_time_occupancy"),
+    path("api/categories/", get_categories, name="get_categories"),  # 🔥 bu satırı ekle
+
 ]
