9b46194e795a2a32faeecea8940c9e334f9c03f0
DaringUnicorn <156504496+daringStu@users.noreply.github.com>
2025-05-10 23:12:13 +0300

    update urls.py

--- campusvision/occupancy/urls.py
+++ campusvision/occupancy/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import RealTimeOccupancyAPIView
+from .views import get_categories
+
 
 urlpatterns = [
     path("api/real-time/", RealTimeOccupancyAPIView.as_view(), name="real_time_occupancy"),
