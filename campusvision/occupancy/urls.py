61179ca43988a384cf7472ba90de15bb2ca8318b
DaringUnicorn <156504496+daringStu@users.noreply.github.com>
2025-05-11 01:42:01 +0300

    find available rooms page is ok

--- campusvision/occupancy/urls.py
+++ campusvision/occupancy/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import RealTimeOccupancyAPIView
 from .views import get_categories
+from .views import get_real_time_occupancy
 
 
 urlpatterns = [
     path("api/real-time/", RealTimeOccupancyAPIView.as_view(), name="real_time_occupancy"),
     path("api/categories/", get_categories, name="get_categories"),  # 🔥 bu satırı ekle
+    path("real-time-json/", get_real_time_occupancy, name="real_time_occupancy"),
 
 ]
