20cf757b6854bccc6462caa7ceb400c29395cde0
İzzettin Furkan Özmen <izzettinfurkan.ozmen@stu.fsm.edu.tr>
2025-05-11 12:22:19 +0300

    Lost & Found Fixed

--- campusvision/lost_and_found/models.py
+++ campusvision/lost_and_found/models.py
@@ -3,13 +3,12 @@ from django.db import models
 class LostItem(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='lost_items/')
+    image = models.ImageField(upload_to='lost_items/', null=True, blank=True)
     location = models.CharField(max_length=200)
-    contact_info = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=100, null=True, blank=True)
     found = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField()
     category = models.CharField(max_length=100, default="other")
-
 
     def __str__(self):
         return self.title
